b9f0321aeaf6584d5e2b8e70e54661ee93eba51e
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2024-08-26 10:59:00 +0530

    task almost complete

--- frontEnd/src/contexts/TaskContext.jsx
+++ frontEnd/src/contexts/TaskContext.jsx
@@ -4,11 +4,21 @@ export const TaskContext = createContext(null)
 
 export default function TaskContextProvider({ children }) {
 	const [tasks, setTask] = useState([])
+
 	function addTask(newDesc) {
-		setTask((value) => [...value, { id: "", desc: newDesc, progress: false }])
+		setTask((value) => [...value, { id: Date.now(), desc: newDesc, progress: false }])
 	}
+
+	function changeProgress(TaskId) {
+		setTask((value) => value.map((item) => item.id === TaskId ? { ...item, progress: !item.progress } : item))
+	}
+
+	function delTask(TaskId) {
+		setTask((value) => value.filter((item) => item.id !== TaskId))
+	}
+
 	return (
-		<TaskContext.Provider value={{ tasks, addTask }}>
+		<TaskContext.Provider value={{ tasks, addTask, changeProgress, delTask }}>
 			{children}
 		</TaskContext.Provider>
 	)
--- frontEnd/src/pages/displayTasks/DisplayTasks.jsx
+++ frontEnd/src/pages/displayTasks/DisplayTasks.jsx
@@ -5,10 +5,18 @@ import { TaskContext } from "../../contexts/TaskContext";
 export default function DisplayTasks() {
 	const [newtask, setNewtask] = useState(false);
 	const [desc, setDesc] = useState("")
-	const { tasks, addTask } = useContext(TaskContext)
+	const { tasks, addTask, changeProgress, delTask } = useContext(TaskContext)
 
 	function handleNewtask() {
 		setNewtask((val) => !val);
+	}
+
+	function deleteTask(id) {
+		delTask(id)
+	}
+
+	function handleProgress(id) {
+		changeProgress(id)
 	}
 
 	function handleChange(e) {
@@ -28,13 +36,15 @@ export default function DisplayTasks() {
 						<div className="task">
 							<h2 className="heading">Progress Heading</h2>
 							{tasks.map((task, index) => (
-								<div className="task" key={index}>
-									<span>{index + 1}.</span><p>TASK: {task.desc}</p>
+								< div className="task" key={index} >
+									<span>{index + 1}.</span><p>id: {task.id}</p>
+									<p>TASK: {task.desc}</p>
 									{(task.progress) ?
 										< p > STATUS : completed</p>
 										:
 										<p>STATUS: NOT completed</p>}
-									<button>DONE</button>
+									<button onClick={() => handleProgress(task.id)}> {task.progress ? "UNDO" : "DONE"}</button>
+									<button onClick={() => deleteTask(task.id)}>DELETE</button>
 								</div>
 							))}
 						</div>
@@ -48,7 +58,7 @@ export default function DisplayTasks() {
 						<div className="formContainer">
 							<label>Description :</label>
 							<input type="text" onChange={(e) => handleChange(e)} />
-							<button onClick={() => handleAdd()} >ADD</button>
+							<button type="submit" onClick={() => handleAdd()} >ADD</button>
 							<button onClick={() => handleNewtask()}>X</button>
 						</div>
 					)}
